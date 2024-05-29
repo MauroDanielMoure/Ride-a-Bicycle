973d578b804d60b7846c6d0b2ebfa1a5fe3850b0
Mauro Daniel Moure <150815145+MauroDanielMoure@users.noreply.github.com>
2024-05-29 16:35:11 -0300

    Add files via upload
    Diseño creado con HTML5, CSS3 y un slide hecho con JavaScript. Mostrando varios escenarios donde se puede disfrutar de andar en bicicleta.

---
+++ RecorreEnBici/code.js
@@ -0,0 +1,35 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.nav');
+const btns = document.querySelectorAll('.nav-btn');
+const slides = document.querySelectorAll ('.video-slide');
+const contents = document.querySelectorAll ('.content');
+
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+let sliderNav = function(manual) {
+    btns.forEach((btn) =>{
+        btn.classList.remove('active');
+    });
+
+    slides.forEach((slides) =>{
+        slides.classList.remove('active');
+    });
+
+    contents.forEach((content) =>{
+        content.classList.remove('active');
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+btns.forEach((btn,i) => {
+    btn.addEventListener('click', () =>{
+        sliderNav(i);
+    })
+})
